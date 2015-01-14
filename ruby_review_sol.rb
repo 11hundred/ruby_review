a7303378375e71aca50d704885dd0b82565023c7
Arjun Venkataswamy <arjun@example.com>
2015-01-14 14:10:38 -0600

    adds solutions

---
+++ ruby_review_sol.rb
@@ -0,0 +1,197 @@
+#### Ruby Review Instructions
+
+# Uncomment the code in each problem to start working on it.
+# Recomment your code from old problems so it doesn't affect
+# your current problem.
+
+
+### 1. USING METHODS
+
+# Given the string "hello", add Ruby code to make just
+# the "h" uppercase
+
+# puts "hello".capitalize
+
+
+
+
+### 2. VARIABLES, OUTPUT AND CONCATENATION
+
+# a) Create two variables - one for your first name and
+# one for your last name.
+# b) Give them both appropriate values.
+# c) Use variables to print out a sentence that looks like:
+      # =>  My name is Arjun Venkataswamy.
+
+# first = "Raghu"
+# last = 'Betina'
+
+# puts "My name is #{first} #{last}"
+
+
+### 3. CREATING METHODS
+
+# a) Write a method called "weather" that:
+#   - takes in two inputs: a city name and a temperature
+#   - prints out a statement that looks like:
+    # => "In Chicago it's -75 degrees."
+# b) Call the above method
+
+# def weather(city, temp)
+#   # city = "Chicago"
+#   # temp = -50
+#   puts "In #{city} it's #{temp} degrees"
+# end
+
+# weather("Chicago", -50)
+
+
+
+
+
+### 4. CONDITIONAL LOGIC
+
+# Write a method called "weather" that:
+#   - takes in one inputs: a temperature
+#   - prints out a statement that depends on the temperature
+#     for example)
+      # => "It is hot hot hot!"
+      # => "Beautiful sweatshirt weather"
+      # => "Don't forget your earmuffs"
+      # => "... just move somewhere else. You don't deserve this."
+
+# def weather(temp)
+#   if temp > 85
+#     puts "It is hot hot hot!"
+#   elsif temp > 55
+#     puts "Beautiful sweatshirt weather"
+#   elsif temp > 15
+#     puts "Don't forget your earmuffs"
+#   else
+#     puts "... just move somewhere else. You don't deserve this."
+#   end
+# end
+
+# weather( rand(100)  )
+
+
+### 5. ARRAYS
+
+# a) Create an array that contains the following strings:
+#    "Hot Doug's"
+#    "Portillo's"
+#    "Kuma's"
+# b) Save the array into a variable
+# c) Using the variable, print out the second item in the array
+
+# chicago_food = [ "Hot Doug's", "Portillo's", "Kuma's"  ]
+
+# puts chicago_food[1]
+
+### 6. HASHES
+
+# a) Pull out Bob's zip code from the following hash:
+
+# bob = { "zip" => 60606, "street" => "155 N Wacker", "city" => "Chicago"  }
+
+# puts bob["zip"]
+
+# challenge) Pull out and print the third person's
+#            street address from the following array:
+
+# people = [
+#   {
+#     "age" => 22,
+#     "sex" => "m",
+#     "location" => {"street" => "222 W Merchandise Mart", "city" => "Chicago", "zip" => 60654}
+#   },
+#   {
+#     "age" => 31,
+#     "sex" => "m",
+#     "location" => {"street" => "155 N Wacker", "city" => "Chicago", "zip" => 60606}
+#   },
+#   {
+#     "age" => 43,
+#     "sex" => "f",
+#     "location" => {"street" => "233 S Wacker", "city" => "Chicago", "zip" => 60606}
+#   }
+# ]
+
+
+# puts people[2]["location"]["street"]
+
+
+### 7. THE EACH LOOP
+
+# a) Loop through the following array, and print
+#    out statements that look like:
+     # => Necco Wafers ruin Halloween
+     # => Smarties ruin Halloween
+
+# worst_candy = ["Necco Wafers", "Smarties", "Candy corn pieces", "Tootsie Rolls", "Now and Laters"]
+# messages = ["ruin Halloween", "are the worst", "are basically inedible"]
+
+# worst_candy.each do |candy|
+#   puts "#{candy} #{messages.sample}"
+# end
+
+# challenge) Randomize the message at the end of each statement
+
+
+### 8. CLASSES
+
+# a) Create a class called Puppy.
+# b) Instantiate a puppy object using this class
+# c) Let the user initialize the puppy with a name attribute
+# d) Automatically initiaize the puppy with a happiness attribute
+# e) Create methods to access the puppy's name and happiness
+# f) Create a method to increase the puppy's happiness by 1
+# g) Create a method to decrease the puppy's happiness by 1
+
+# class Puppy
+#   def initialize(name)
+#     @name = name
+#     @happiness = 5
+#   end
+
+#   def name
+#     return @name
+#   end
+
+#   def happiness
+#     puts "Happiness is now #{@happiness}"
+#   end
+
+#   def feed
+#     @happiness += 1
+#     happiness
+#   end
+
+#   def kick
+#     @happiness -= 1
+#     happiness
+#   end
+# end
+
+
+# puppy1 = Puppy.new("Mr. Taco")
+# puppy1.kick
+# puppy1.kick
+
+
+
+
+# 9. CHALLENGE: RUBY TO HTML
+
+# Create a Ruby script that takes the following array of image links
+# and turns them into images on a web page. Bonus if you use Bootstrap
+
+# images = [
+#   "http://uhaweb.hartford.edu/aschmidt/kitten12.jpg",
+#   "http://images2.fanpop.com/image/photos/9700000/i-love-kittyyyyyyyyyyyyy-cute-kittens-9785244-300-300.jpg",
+#   "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=8069015",
+#   "http://www.petmedicalcenter.com/wp-content/uploads/kitten_wellness_program-300x300.jpg"
+# ]
+
+
+
